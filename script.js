bd7a1e7678f82de63685c9956a8efd0470c96e0d
josephboat404 <josephboatengcollege45@gmail.com>
2023-08-13 12:23:53 -0400

    Improve SM screen Respon

--- script.js
+++ script.js
@@ -1,44 +1,31 @@
 let generatedPassword = "404";
-const inputElement = document.getElementById("myInput");
-
-const buttonforcopy = document.getElementById("buttonforcopy");
-const buttonfornewpass = document.getElementById("buttonfornewpass");
+const inputElement = document.querySelector(".displaygen");
+const inputElement1 = document.querySelector(".displaygen1");
+const copyButtons = document.querySelectorAll(".buttonforcopy");
+const newPassButtons = document.querySelectorAll(".buttonfornewpass");
 
 function validatorpass() {
   let passsubmitted = document.forms["myForm"]["unlockpage"].value;
   passsubmitted = passsubmitted.trim();
 
   if (passsubmitted === generatedPassword) {
-    // alert("Password matches");
     return true;
   } else {
-    // alert("Password does not match");
     return false;
   }
 }
 
-function NextButtonClick() {
-  let element = document.querySelector(".IamJoseph");
-  if (element.innerHTML.trim() === "I am<br>Joseph".trim()) {
-    element.innerHTML =
-      "A computer science <br> student with passion <br> for cybersecurity and <br/>web development";
-    element.style.fontSize = "20px";
-  } else {
-    element.innerHTML = "I am<br>Joseph";
-    element.style.fontSize = "84px";
-  }
-  console.log("called");
-}
+
 let element = document.querySelector(".IamJoseph");
 console.log(element.innerHTML.trim());
 
-function CopyButtonClick() {
-  const myInput = document.getElementById("myInput");
+function CopyButtonClick(event) {
+  const myInput = event.target.closest(".col-md").querySelector(".displaygen");
   const textToCopy = myInput.innerHTML.replace(/;/g, "");
   navigator.clipboard.writeText(textToCopy);
 }
 
-function NewPassButtonClick() {
+function NewPassButtonClick(event) {
   const passwordLength = 14;
   let password = "";
   const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -52,15 +39,19 @@ function NewPassButtonClick() {
     password += allCharacters[randomIndex];
   }
   inputElement.innerHTML = password;
+  inputElement1.innerHTML = password;
+
   generatedPassword = password;
   console.log(password);
 }
 
+copyButtons.forEach(function (button) {
+  button.addEventListener("click", CopyButtonClick);
+});
 
-buttonforcopy.addEventListener("click", CopyButtonClick);
-buttonfornewpass.addEventListener("click", NewPassButtonClick);
-
-
+newPassButtons.forEach(function (button) {
+  button.addEventListener("click", NewPassButtonClick);
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   function showInitialMessage() {
@@ -71,23 +62,23 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   showInitialMessage();
 
-
   const myToast = new bootstrap.Toast(document.getElementById("myToast"));
-
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  var copyButton = document.getElementById('buttonforcopy');
-  var tooltip = new bootstrap.Tooltip(copyButton, {
-    trigger: 'manual',
-    title: 'Copied!',
-    placement: 'top'
-  });
+  var copyButtons = document.querySelectorAll('.buttonforcopy');
+  copyButtons.forEach(function (copyButton) {
+    var tooltip = new bootstrap.Tooltip(copyButton, {
+      trigger: 'manual',
+      title: 'Copied!',
+      placement: 'top'
+    });
 
-  copyButton.addEventListener('click', function () {
-    tooltip.show();
-    setTimeout(function () {
-      tooltip.hide();
-    }, 2000);
+    copyButton.addEventListener('click', function () {
+      tooltip.show();
+      setTimeout(function () {
+        tooltip.hide();
+      }, 2000);
+    });
   });
 });
